5114a8e37c2c9d9900132c5eb1e2b35ac3f8a09d
chandre21cn <chandre21cn@gmail.com>
2026-09-10 23:23:59 +0800

    add Icon component

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,25 @@
+<template>
+    <svg aria-hidden="true" :style="styles">
+        <use :href="symbolId" :fill="color" />
+    </svg>
+</template>
+
+<script lang="ts" setup>
+    import { computed } from 'vue'
+    
+    const props = defineProps({
+        prefix: { type: String, default: 'icon' },
+        name: { type: String, required: true },
+        color: { type: String, default: 'currentColor' },
+        size: { type: [ Number, String ], default: 14  }
+    })
+
+    const styles = computed(() => {
+        let size = typeof props.size == 'string' ? props.size : props.size + 'px'
+        return { width: size, height: size }
+    })
+
+    const symbolId = computed(() => {
+        return `#${props.prefix}-${props.name}`
+    })
+</script>
--- src/components/Splitter/Splitter.vue
+++ src/components/Splitter/Splitter.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { SplitterGroup, SplitterPanel, SplitterResizeHandle } from 'reka-ui'
-
 </script>
 
 <template>
